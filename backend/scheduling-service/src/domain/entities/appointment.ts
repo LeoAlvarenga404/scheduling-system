e83cfe489f7aca17c0afbc327783e7bd12c973e6
Leonardo Prado <leonardo.palvarenga@gmail.com>
2026-03-01 20:52:22 -0300

    feat: create appointment entity

---
+++ backend/scheduling-service/src/domain/entities/appointment.ts
@@ -0,0 +1,38 @@
+import { Entity } from "../core/entities/entity";
+import { UniqueEntityID } from "../core/entities/unique-entity-id";
+
+export interface AppointmentProps {
+  tenantId: string;
+  customerId: string;
+  professionalId: string;
+  scheduledAt: Date;
+  status: "PENDING" | "CONFIRMED" | "CANCELLED" | "COMPLETED";
+}
+
+export class Appointment extends Entity<AppointmentProps> {
+  get tenantId() {
+    return this.props.tenantId;
+  }
+
+  get customerId() {
+    return this.props.customerId;
+  }
+
+  get professionalId() {
+    return this.props.professionalId;
+  }
+
+  get scheduledAt() {
+    return this.props.scheduledAt;
+  }
+
+  get status() {
+    return this.props.status;
+  }
+
+  static create(_props: AppointmentProps, id?: UniqueEntityID) {
+    const appointment = new Appointment({ ..._props }, id);
+
+    return appointment;
+  }
+}
